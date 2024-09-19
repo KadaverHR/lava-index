9a9a53a02cf542f0d72ad71aa0120689336c6ec1
KadaverHR <hasslicheratte@yandex.ru>
2024-09-19 14:59:01 +0300

    start

---
+++ src/assets/js/app.js
@@ -0,0 +1,27 @@
+
+
+$(document).ready(function () {
+
+  let burger = document.querySelector('#hamburger-menu-mob');
+  let menu = document.querySelector('#catalog-drop-mob');
+  let menuLinks = menu.querySelectorAll('.catalog-link');
+  let closeMenu = menu.querySelector('#close-mob')
+
+  burger.addEventListener('click', function () {
+    menu.classList.toggle('active');
+    document.body.classList.toggle('stop-scroll');
+  });
+
+  closeMenu.addEventListener('click', function () {
+    menu.classList.toggle('active');
+    document.body.classList.toggle('stop-scroll');
+  });
+
+  menuLinks.forEach(function (el) {
+    el.addEventListener('click', function () {
+      menu.classList.remove('active');
+      document.body.classList.remove('stop-scroll')
+    })
+  });
+
+})
---
+++ src/assets/libs/floorsArray.js
@@ -0,0 +1,15 @@
+
+
+let floorsArray = (thisCoutFloor) => {
+	console.log(thisCoutFloor + 'asdasd')
+	let hakaList = [`stone`, `miniBoss`, null, 'door', `stone`,
+		null, null, null, null, null,
+		null, null, `boss`, null, `boss`,
+		`boss`, null, null, null, null,
+		null, null, `boss`, null, `miniBoss`,
+		null, `stone`, null, null, null,
+		`miniBoss`, null, `bigBoss`, null, null,
+		'statr', null, `stone`, null, `stone`]
+
+	return hakaList
+}
